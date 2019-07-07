d1d83ea582674369afbcb37230e4336e2d77f729
sapir and eti <sapirkalbin@gmail.com>
2019-07-07 21:12:55 +0300

    first commit

---
+++ AlefAdTav/BoardViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BoardViewController.swift
+//  AlefAdTav
+//
+//  Created by Sapir Kalbin on 07/07/2019.
+//  Copyright © 2019 Sapir Kalbin. All rights reserved.
+//
+
+import UIKit
+
+
+class SubjectViewController: UIViewController {
+    var boyOrGirl = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+    func setGender(boyOrGirl: String) {
+        self.boyOrGirl = boyOrGirl
+        
+    }
+    
+}
+
+
---
+++ AlefAdTav/SubjectViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SubjectViewController.swift
+//  AlefAdTav
+//
+//  Created by Sapir Kalbin on 07/07/2019.
+//  Copyright © 2019 Sapir Kalbin. All rights reserved.
+//
+
+import UIKit
+
+class SubjectViewController: UIViewController {
+    var gender = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+    
+    func setGender(boyOrGirl: String)
+    {
+        gender = boyOrGirl
+    }
+
+
+}
